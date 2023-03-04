fd7e357f63391c2b0cd0174e66c726c09b16fe7b
Samira <samira.azam1706@gmail.com>
2023-03-04 11:43:52 +0600

    radio buttons component added

--- src/components/RadioButtons.jsx
+++ src/components/RadioButtons.jsx
@@ -6,25 +6,27 @@ const RadioButtons = (props) => {
   const { label, name, options, ...rest } = props;
   return (
     <div className="form-control">
-      <label htmlFor={name}>{label}</label>
-      <Field>
+      <label>{label}</label>
+      <Field name={name}>
         {({ field }) => {
-          return options.map((option) => (
-            <React.Fragment key={option.key}>
-              <input
-                type="radio"
-                id={option.value}
-                {...rest}
-                {...field}
-                value={option.value}
-                checked={field.value === option.value}
-              />
-              <label htmlFor={option.value}>{option.key}</label>
-            </React.Fragment>
-          ));
+          return options.map((option) => {
+            return (
+              <React.Fragment key={option.key}>
+                <input
+                  type="radio"
+                  id={option.value}
+                  {...field}
+                  {...rest}
+                  value={option.value}
+                  checked={field.value === option.value}
+                />
+                <label htmlFor={option.value}>{option.key}</label>
+              </React.Fragment>
+            );
+          });
         }}
       </Field>
-      <ErrorMessage name={name} component={TextError} />
+      <ErrorMessage component={TextError} name={name} />
     </div>
   );
 };
